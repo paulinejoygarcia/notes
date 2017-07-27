683f39d653b814ea398cf019d6e725fe54089c5c
Pauline Joy Garcia <paulinejoygarcia@gmail.com>
2017-07-27 18:23:31 +0800

    Final Commit

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -17,7 +17,7 @@ export const NoteList = (props) => {
     });
   };
   return (
-    <div>
+    <div className="item-list">
       <NoteListHeader/>
       {props.notes.length === 0 ? <NoteListEmptyItem/> : undefined}
       {renderList(props.notes)}
